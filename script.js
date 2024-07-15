d746771fa1033f032fd55e60a4466f1f9f4860a0
echambers02 <130339397+echambers02@users.noreply.github.com>
2024-07-15 17:53:36 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,91 +1,165 @@
-/* Copyright 2023 The MediaPipe Authors.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-     http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License. */
-import { MDCTextField } from "https://cdn.skypack.dev/@material/textfield";
-import {
-  TextClassifier,
-  FilesetResolver,
-} from "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-text@0.10.0";
-const textField = new MDCTextField(document.querySelector(".mdc-text-field"));
-const defaultText =
-  "But soft, what light through yonder window breaks? \n \
-It is the East, and Juliet is the sun.\n \
-Arise, fair sun, and kill the envious moon,\n \
-Who is already sick and pale with grief\n \
-That thou, her maid, art far more fair than she.\n \
-Be not her maid since she is envious.\n \
-Her vestal livery is but sick and green,\n \
-And none but fools do wear it. Cast it off.\n \
-It is my lady. O, it is my love!\n \
-O, that she knew she were!\n othing. What of that?\n \
-Her eye discourses; I will answer it.";
-// Get the required elements
-const input = document.getElementById("input");
-const output = document.getElementById("output");
-const submit = document.getElementById("submit");
-const defaultTextButton = document.getElementById("populate-text");
+// Copyright 2023 The MediaPipe Authors.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+import { PoseLandmarker, FilesetResolver, DrawingUtils } from "https://cdn.skypack.dev/@mediapipe/tasks-vision@0.10.0";
 const demosSection = document.getElementById("demos");
-let textClassifier;
-// Create the TextClassifier object upon page load
-const createTextClassifier = async () => {
-  const text = await FilesetResolver.forTextTasks(
-    "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-text@0.10.0/wasm",
-  );
-  textClassifier = await TextClassifier.createFromOptions(text, {
-    baseOptions: {
-      modelAssetPath: `https://storage.googleapis.com/mediapipe-models/text_classifier/bert_classifier/float32/1/bert_classifier.tflite`,
-    },
-    maxResults: 5,
-  });
-  // Show demo section now model is ready to use.
-  demosSection.classList.remove("invisible");
+let poseLandmarker = undefined;
+let runningMode = "IMAGE";
+let enableWebcamButton;
+let webcamRunning = false;
+const videoHeight = "360px";
+const videoWidth = "480px";
+// Before we can use PoseLandmarker class we must wait for it to finish
+// loading. Machine Learning models can be large and take a moment to
+// get everything needed to run.
+const createPoseLandmarker = async () => {
+    const vision = await FilesetResolver.forVisionTasks("https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.0/wasm");
+    poseLandmarker = await PoseLandmarker.createFromOptions(vision, {
+        baseOptions: {
+            modelAssetPath: `https://storage.googleapis.com/mediapipe-models/pose_landmarker/pose_landmarker_lite/float16/1/pose_landmarker_lite.task`,
+            delegate: "GPU"
+        },
+        runningMode: runningMode,
+        numPoses: 2
+    });
+    demosSection.classList.remove("invisible");
 };
-createTextClassifier();
-// Add a button click listener to add the default text
-defaultTextButton.addEventListener("click", () => {
-  input.value = defaultText;
-});
-// Add a button click listener that classifies text on click
-submit.addEventListener("click", async () => {
-  if (input.value === "") {
-    alert("Please write some text, or click 'Populate text' to add text");
-    return;
-  }
-  output.innerText = "Classifying...";
-  await sleep(5);
-  const result = textClassifier.classify(input.value);
-  displayClassificationResult(result);
-});
-function sleep(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
+createPoseLandmarker();
+/********************************************************************
+// Demo 1: Grab a bunch of images from the page and detection them
+// upon click.
+********************************************************************/
+// In this demo, we have put all our clickable images in divs with the
+// CSS class 'detectionOnClick'. Lets get all the elements that have
+// this class.
+const imageContainers = document.getElementsByClassName("detectOnClick");
+// Now let's go through all of these and add a click event listener.
+for (let i = 0; i < imageContainers.length; i++) {
+    // Add event listener to the child element whichis the img element.
+    imageContainers[i].children[0].addEventListener("click", handleClick);
 }
-// Iterate through the sentiment categories in the TextClassifierResult object, then display them in #output
-function displayClassificationResult(result) {
-  if (result.classifications[0].categories.length > 0) {
-    output.innerText = "";
-  } else {
-    output.innerText = "Result is empty";
-  }
-  const categories = [];
-  // Single-head model.
-  for (const category of result.classifications[0].categories) {
-    const categoryDiv = document.createElement("div");
-    categoryDiv.innerText = `${category.categoryName}: ${category.score.toFixed(2)}`;
-    // highlight the likely category
-    if (category.score.toFixed(2) > 0.5) {
-      categoryDiv.style.color = "#12b5cb";
+// When an image is clicked, let's detect it and display results!
+async function handleClick(event) {
+    if (!poseLandmarker) {
+        console.log("Wait for poseLandmarker to load before clicking!");
+        return;
     }
-    output.appendChild(categoryDiv);
-    window.max.outlet(category.categoryName, category.score.toFixed(2));
-  }
+    if (runningMode === "VIDEO") {
+        runningMode = "IMAGE";
+        await poseLandmarker.setOptions({ runningMode: "IMAGE" });
+    }
+    // Remove all landmarks drawed before
+    const allCanvas = event.target.parentNode.getElementsByClassName("canvas");
+    for (var i = allCanvas.length - 1; i >= 0; i--) {
+        const n = allCanvas[i];
+        n.parentNode.removeChild(n);
+    }
+    // We can call poseLandmarker.detect as many times as we like with
+    // different image data each time. The result is returned in a callback.
+    poseLandmarker.detect(event.target, (result) => {
+        const canvas = document.createElement("canvas");
+        canvas.setAttribute("class", "canvas");
+        canvas.setAttribute("width", event.target.naturalWidth + "px");
+        canvas.setAttribute("height", event.target.naturalHeight + "px");
+        canvas.style =
+            "left: 0px;" +
+                "top: 0px;" +
+                "width: " +
+                event.target.width +
+                "px;" +
+                "height: " +
+                event.target.height +
+                "px;";
+        event.target.parentNode.appendChild(canvas);
+        const canvasCtx = canvas.getContext("2d");
+        const drawingUtils = new DrawingUtils(canvasCtx);
+        for (const landmark of result.landmarks) {
+            drawingUtils.drawLandmarks(landmark, {
+                radius: (data) => DrawingUtils.lerp(data.from.z, -0.15, 0.1, 5, 1)
+            });
+            drawingUtils.drawConnectors(landmark, PoseLandmarker.POSE_CONNECTIONS);
+        }
+    });
+}
+/********************************************************************
+// Demo 2: Continuously grab image from webcam stream and detect it.
+********************************************************************/
+const video = document.getElementById("webcam");
+const canvasElement = document.getElementById("output_canvas");
+const canvasCtx = canvasElement.getContext("2d");
+const drawingUtils = new DrawingUtils(canvasCtx);
+// Check if webcam access is supported.
+const hasGetUserMedia = () => { var _a; return !!((_a = navigator.mediaDevices) === null || _a === void 0 ? void 0 : _a.getUserMedia); };
+// If webcam supported, add event listener to button for when user
+// wants to activate it.
+if (hasGetUserMedia()) {
+    enableWebcamButton = document.getElementById("webcamButton");
+    enableWebcamButton.addEventListener("click", enableCam);
+}
+else {
+    console.warn("getUserMedia() is not supported by your browser");
+}
+// Enable the live webcam view and start detection.
+function enableCam(event) {
+    if (!poseLandmarker) {
+        console.log("Wait! poseLandmaker not loaded yet.");
+        return;
+    }
+    if (webcamRunning === true) {
+        webcamRunning = false;
+        enableWebcamButton.innerText = "ENABLE PREDICTIONS";
+    }
+    else {
+        webcamRunning = true;
+        enableWebcamButton.innerText = "DISABLE PREDICTIONS";
+    }
+    // getUsermedia parameters.
+    const constraints = {
+        video: true
+    };
+    // Activate the webcam stream.
+    navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
+        video.srcObject = stream;
+        video.addEventListener("loadeddata", predictWebcam);
+    });
+}
+let lastVideoTime = -1;
+async function predictWebcam() {
+    canvasElement.style.height = videoHeight;
+    video.style.height = videoHeight;
+    canvasElement.style.width = videoWidth;
+    video.style.width = videoWidth;
+    // Now let's start detecting the stream.
+    if (runningMode === "IMAGE") {
+        runningMode = "VIDEO";
+        await poseLandmarker.setOptions({ runningMode: "VIDEO" });
+    }
+    let startTimeMs = performance.now();
+    if (lastVideoTime !== video.currentTime) {
+        lastVideoTime = video.currentTime;
+        poseLandmarker.detectForVideo(video, startTimeMs, (result) => {
+            canvasCtx.save();
+            canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
+            for (const landmark of result.landmarks) {
+                drawingUtils.drawLandmarks(landmark, {
+                    radius: (data) => DrawingUtils.lerp(data.from.z, -0.15, 0.1, 5, 1)
+                });
+                drawingUtils.drawConnectors(landmark, PoseLandmarker.POSE_CONNECTIONS);
+            }
+            canvasCtx.restore();
+        });
+    }
+    // Call this function again to keep predicting when the browser is ready.
+    if (webcamRunning === true) {
+        window.requestAnimationFrame(predictWebcam);
+    }
 }
